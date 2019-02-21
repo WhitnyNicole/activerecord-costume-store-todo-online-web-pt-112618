90c6ffa24eaf95dc0594374acae20742fba3c39e
Whitny Edwards <wne4e2@gmail.com>
2019-02-21 00:49:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name 
       t.integer :price 
-      t.size :size 
+      t.integer :size 
       t.string :image_url 
       t.timestamp
       t.timestamp 
