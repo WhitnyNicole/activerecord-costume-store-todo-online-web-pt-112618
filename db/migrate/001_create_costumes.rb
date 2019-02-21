625218adf45633d25ece2f78df0a8c5ac780e8aa
Whitny Edwards <wne4e2@gmail.com>
2019-02-21 00:47:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,3 +7,14 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+  def change
+    create_table :costumes do |t|
+      t.string :name 
+      t.integer :price 
+      t.size :size 
+      t.string :image_url 
+      t.timestamp
+      t.timestamp 
+    end 
+  end 
